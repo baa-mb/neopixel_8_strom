2179829029ddcca93ed4cfeb5e54c9f8a5bf8ed6
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-11 00:07:25 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
+let farb_index = 0
+let farbe = 0
+let hell = 0
 basic.showIcon(IconNames.Yes)
 let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-let hell = 0
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.setBrightness(hell)
+strip.showColor(neopixel.colors(NeoPixelColors.White))
+let farbe_alt = NeoPixelColors.White
+let liste = [NeoPixelColors.White, 16711680, 255, 16776960, 65280, 16753920]
+let farb_anzahl = liste.length
 basic.forever(function () {
-    hell += 1
-    hell = hell % 10
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    hell = (hell + 1) % 10
+    farbe = liste[farb_index]
+    strip.showColor(farbe)
+    strip.setPixelColor(3, farbe_alt)
+    strip.setPixelColor(4, farbe_alt)
     strip.setBrightness(hell * 25)
     strip.show()
+    if (hell == 0) {
+        farbe_alt = farbe
+        farb_index = (farb_index + 1) % farb_anzahl
+    }
     basic.showNumber(hell)
-    basic.pause(2000)
+    basic.pause(1000)
 })
